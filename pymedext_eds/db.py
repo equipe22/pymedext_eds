50fd787a2568054cf7915cc0f2da97bf0a02397a
aneuraz <cse180025@bbs-edsi-p008.bbs.aphp.fr>
2020-09-14 14:28:56 +0200

    add note_ids filter

--- pymedext_eds/db.py
+++ pymedext_eds/db.py
@@ -7,22 +7,27 @@ def get_engine():
     password = pgpasslib.getpass('10.172.28.101', 5432, 'coronaomop', 'coronascientist')
     return create_engine(f'postgresql+psycopg2://coronascientist:{password}@10.172.28.101:5432/coronaomop')
 
-def construct_query(limit, min_date, view='unstable'): 
+
+def construct_query(limit, min_date='2020-03-01', view='unstable', note_ids = None): 
     if limit != -1:
         limit = "LIMIT {}".format(limit)
     else:
         limit = ""
         
-    query = """
-        SELECT note_id, person_id, note_text
-        FROM {}.note 
-        WHERE note_date > '{}'
-        AND note_text IS NOT NULL OR note_text not in ('','\n') 
-        {}
-        """.format(view, min_date, limit)
+    notes = ""
+    if note_ids is not None:
+        notes = f"AND note_id IN {tuple(note_ids)}"
+        
+    query = f"""
+        SELECT note_id, person_id, note_text, note_date
+        FROM {view}.note 
+        WHERE note_date > '{min_date}'
+        {notes}
+        AND note_text IS NOT NULL OR note_text not in ('','\n', ' ') 
+        {limit}
+        """
     #logger.debug('query set with parameters: view = {}, min_date= {}, limit={}'.format(view, min_date, limit))
     return query
-
 
 def rawtext_pg(limit, min_date,  engine,chunksize=100):
     query = construct_query(limit, min_date)
