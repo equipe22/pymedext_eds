5c689b4bb329e76d9794ef9fea89c6a9f22f4d3a
aneuraz <cse180025@bbs-edsi-p008.bbs.aphp.fr>
2020-09-18 16:20:42 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pymedext_eds',
+    version='0.1dev',
+    packages=['pymedext_eds',],
+    license='MIT',
+    #long_description=open('README.txt').read(),
+)
