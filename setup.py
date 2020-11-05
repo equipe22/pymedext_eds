9ab6b335ca95b24b5c5b63865441a5f8be7a193d
aneuraz <cse180025@bbs-edsi-p008.bbs.aphp.fr>
2020-11-05 10:14:25 +0100

    fix install

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pymedext_eds',
     version='0.1dev',
-    packages=['pymedext_eds',],
+    packages=['pymedext_eds','pymedext_eds.extract','pymedext_eds.pyromedi'],
     license='MIT',
     #long_description=open('README.txt').read(),
 )
