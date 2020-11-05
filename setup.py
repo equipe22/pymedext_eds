48869ad3d37d485d4bbed45e78681eadffb2339d
aneuraz <cse180025@bbs-edsi-p008.bbs.aphp.fr>
2020-11-05 14:49:21 +0100

    add demo data and romedi cache

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version='0.1dev',
     packages=['pymedext_eds','pymedext_eds.extract','pymedext_eds.pyromedi'],
     license='MIT',
+    package_data={'demo': ['data/demo/*.txt'],'romedi': ['data/romedi/*.p']}
     #long_description=open('README.txt').read(),
 )
