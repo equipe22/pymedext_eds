056764cdf75811142a35285bce2de26e1318c4a7
Antoine Neuraz <antoine@iMac-de-Antoine-2.local>
2020-11-23 16:12:06 +0100

    fix entity visualization

--- pymedext_eds/viz.py
+++ pymedext_eds/viz.py
@@ -1,49 +1,58 @@
 from spacy import displacy
+import re
 
-def get_dict_annotations(document, _type, source_id = None, target_id = None):
-        res = []
-        for anno in document['annotations']:
-            if source_id is not None: 
-                if anno['source_ID'] != source_id:
-                    continue
-            if target_id is not None:
-                if anno['target_ID'] != target_id:
-                    continue
-            if anno['type'] == _type:
-                res.append(anno)
-        return res
+# def get_dict_annotations(document, _type, source_id = None, target_id = None):
+#         res = []
+#         for anno in document.annotations:
+#             if source_id is not None: 
+#                 if anno.source_ID != source_id:
+#                     continue
+#             if target_id is not None:
+#                 if anno.target_ID != target_id:
+#                     continue
+#             if anno.type == _type:
+#                 res.append(anno)
+#         return res
 
-def convert_to_displacy(document, entity_type, source_id, attributes):
+def convert_to_displacy(document, entity_type, attributes, label_key = 'label'):
     ents= []
-    annots = get_dict_annotations(document, entity_type, source_id = source_id)
+    annots = document.get_annotations(entity_type)
     #annots = [x for x in document if x['type'] in entity_type and x['source_ID'] == source_id]
     if len(annots) > 0:
+        annots = document.get_annotations(entity_type)
+        ents= []
         for annot in annots:
-            ents.append({"start": annot['span'][0], 'end':annot['span'][1], "label": entity_type.upper()})
-            drug_id = annot['id']
-            if attributes != []:
-                for att, val in annot['attributes'].items(): 
+            if label_key in annot.attributes.keys():
+                label = annot.attributes[label_key]
+            else: 
+                label = entity_type.upper()
+            if attributes is not None:
+                for att, val in annot.attributes.items(): 
                     if att in attributes:
-                        for v in val:
-                            ents.append({"start":v['span'][0],'end':v['span'][1], 'label':att.upper()})
+                        label += f'/{val}'
+            ents.append({"start": annot.span[0], 'end':annot.span[1], "label": label})
+            drug_id = annot.ID
+            
     return ents
 
 
-def display_annotations(document, root = "sentence", entities = ["ENT/DRUG", "ENT/DOSE"], attributes = ['ENT/DOSE'],
-                        palette = [ '#ffb3ba','#ffdfba','#ffffba','#baffc9','#bae1ff']):
+
+
+def display_annotations(document,  entities = ["ENT/DRUG", "ENT/DOSE"], attributes = None,
+                        palette = [ '#ffb3ba','#ffdfba','#ffffba','#baffc9','#bae1ff'], 
+                       label_key = 'label', jupyter = True):
 
     to_disp = []
     tmp = { "ents": []}
 
-    for sent in get_dict_annotations(document,root):
+    for entity in entities:
+        conv_ents = convert_to_displacy(document, entity, attributes = attributes, label_key = label_key)
+        tmp['ents'] += conv_ents
 
-        for entity in entities:
-            conv_ents = convert_to_displacy(document, entity, sent['id'], attributes = attributes)
-            tmp['ents'] += conv_ents
-
-    tmp['text'] = document['annotations'][0]['value']
+    tmp['text'] = document.annotations[1].value
     tmp['uuid'] = 0
     
+    tmp['text'] = re.sub('\. ', '\n ', tmp['text'])
      
     options = {"colors" : {}}
     i = 0
@@ -51,5 +60,4 @@ def display_annotations(document, root = "sentence", entities = ["ENT/DRUG", "EN
         options['colors'][entity.upper()] = palette[i]
         i += 1
 
-    displacy.render(tmp, manual=True, style = 'ent', options = options , jupyter=True)
-
+    displacy.render(tmp, manual=True, style = 'ent', options = options , jupyter=jupyter)
