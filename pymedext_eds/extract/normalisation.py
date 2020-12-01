31c3ef6e9a0275244f145ececa804fa3525a0415
aneuraz <cse180025@bbs-edsi-p008.bbs.aphp.fr>
2020-12-01 15:25:56 +0100

    clean constants

--- pymedext_eds/extract/normalisation.py
+++ pymedext_eds/extract/normalisation.py
@@ -4,9 +4,13 @@ import re
 from collections import Counter
 
 
+from .constants import NUMBER, FREQ_REPLACE, FREQ_EXP, NUMBER_4_DOSE, UNITS
+
+
 #####DRUG NORMALIZATION
 def clean_drug(entity, romedi): 
     for field in ['BN_label', 'PIN_label', 'IN_label', 'IN_hidden_label', 'BN_hidden_label']:
+        entity = unidecode(entity)
         if field in ['IN_hidden_label', 'BN_hidden_label']: 
             str_to_match =  entity.lower()
         else: 
@@ -16,7 +20,9 @@ def clean_drug(entity, romedi):
 
         if len(match)> 0: 
             res = match[0]
-            break
+            res = { k: res[k] for k in ['BN_label', 'PIN_label', 'IN_label', 'ATC7','ATC5','ATC4'] }
+            break     
+            
         else: 
             res = {}
     return res
@@ -32,126 +38,11 @@ def clean_class(entity, class_norm):
         return []
 
 
-###FREQ DOSE NORMALIZATION
-
-# def fusion_norm_freq(drugblob):
-#     if drugblob["ENT/FREQ"]:
-#         freq_mention = [t["mention"] for t in drugblob["ENT/FREQ"]]
-#         freq = [ clean_freq(t)[0] for t in freq_mention]
-#     else:
-#         freq = []
-#         freq_mention = []
-
-#     if drugblob["ENT/DOSE"]:
-#         dose_mention = [t["mention"] for t in drugblob["ENT/DOSE"]]
-#         dose = [ clean_dose(t)[0] for t in dose_mention]
-#     else:
-#         dose = []
-#         dose_mention = []
-        
-
-#     blob_norm = []
-#     for freq, (value, unit) in zip(freq, [t for sub in dose for t in sub]):
-#         blob_norm.append((value, unit, freq))
-        
-        
-#     return blob_norm, freq_mention, dose_mention
-
-number = {'zero': '0',
- 'une ': '1 ',
- 'un ': '1 ',
-
- 'deux': '2',
- 'trois': '3',
- 'quatre': '4',
- 'cinq': '5',
- 'six': '6',
- 'sept': '7',
- 'huit': '8',
- 'neuf': '9',
- 'dix': '10',
- 'onze': '11',
- 'douze': '12',
- 'treize': '13',
- 'quatorze': '14',
- 'quinze': '15',
- 'seize': '16',
- 'dix-sept': '17',
- 'dix-huit': '18',
- 'dix-neuf': '19'}
-
-freq_replace = {"les ":" ",
-                "le ":" ",
-                "par ":" ",
-                " et ":" ",
-                "a ":" ",
-                "fois ":" ",
-                "x ":" ",
-                "^jr?$":"jour",
-                "jours":"jour",
-                r'(\d) ?jr?$': r'\g<1> jour',
-                '(\d) ?h(\s|$)':'\g<1> heure\g<2>',
-                r'x ?(\d)': r'\g<1>',
-                "heures":"heure",
-                "semaines":"semaine",
-
-                "toutes|tous|toute":"tout"
-               }
-
-freq_exp = OrderedDict({
-    "error":["0 midi", "1 .0.0", "garde veine"],
-    "J_1/2":["1 jour sur 2", "tout 2 jour"],
-
-    "H_1 1 1 1":["3 4 jour", "4 jour", "tout 6 heure", "6 heure", "^1 1 1 1$"],
-    "H_1 1 0 1":["matin midi soir", "3 jour", "tout 8 heure", "8 heure", "^1 1 1$"],
-    "H_1 0 0 1":["(1 )?matin soir", "2 jour", "^1 0 1$"],
-    "H_1 1 0 0":["matin midi", "^1 1 0$"],
-    "H_0 1 0 1":["midi soir", "^0 0 1$"],
-    "H_0 0 0 1":["(1 )?soir",  "au coucher", "20 00", "^0 0 1$"],
-    "H_0 1 0 0":["midi", "^0 1 0$"],
-    "H_0 0 1 0":["au gouter"],
-
-    "H_2 2 2 2":["^2 2 2 2$"],
-    "H_2 2 0 2":["^2 2 2$"],
-    "H_2 0 0 2":[ "^2 0 2$"],
-    "H_2 2 0 0":[ "^2 2 0$"],
-    "H_0 0 0 2":[ "^0 0 2$"],
-    "H_0 2 0 0":[ "^0 2 0$"],
-    "H_2 0 0 0":[ "^2 0 0$"],
-    "H_3 3 3 3":["^3 3 3 3$"],
-    "H_3 3 0 3":["^3 3 3$"],
-    "H_3 0 0 3":[ "^3 0 3$"],
-    "H_3 3 0 0":[ "^3 3 0$"],
-    "H_0 0 0 3":[ "^0 0 3$"],
-    "H_0 3 0 0":[ "^0 3 0$"],
-    "H_3 0 0 0":[ "^3 0 0$"],
-
-
-    #semaine
-    "S_1/2":["tout 2 semaine", "tout 15 jour"],
-    "S_1/3":["tout 3 semaine"],
-    "S_1/6":["tout 6 semaine"],
-    "S_1/8":["tout 2 mois", "2 mois", "tout 8 semaine"],
-    "S_1/13":["tout 3 mois", "3 mois", "tout 12 semaine", "trimestrielle"],
-    "S_1/26":["tout 6 mois", "6 mois", "semestrielle"],
-    "S_1/17":["tout 4 mois", "4 mois"],
-    
-    #jour
-    "J_3/7":["3 semaine"],
-
-    #unspecifix, FP generation
-    "S_1/4":["tout mois", "mois", "tout 4 semaine", "mensuelle"],
-    "J_1/7":["(1 )?semaine", "hebdomadaire", " lundi|mardi|mercredi|jeudi|vendredi|samedi|dimanche"],
-    "H_1 0 0 0":["(1 )?matin", "tout jour", "(1 )?jour", "tout 24 heure", "24 heure", "09 00",  "^1 0 0$"],
-
-}
-)
-
-unfolded_freq = [t for sub in [[(k, u) for u in v] for k, v in freq_exp.items()] for t in sub]
 
 
 def norm_freq(string):
-    freq = "NA"
+    unfolded_freq = [t for sub in [[(k, u) for u in v] for k, v in FREQ_EXP.items()] for t in sub]
+    freq = None
     for key, pattern in unfolded_freq:
         if re.search(pattern, string):
             freq = key
@@ -175,10 +66,10 @@ def clean_freq(string):
     string = string.strip().lower()
     
     #replace number
-    for k, v in number.items():
+    for k, v in NUMBER.items():
         string = re.sub(k, v, string)
     #replace stop words
-    for k, v in freq_replace.items():
+    for k, v in FREQ_REPLACE.items():
         string = re.sub(k, v, string)
 
     string = re.sub(' +', ' ', string)
@@ -193,66 +84,7 @@ def clean_freq(string):
 ############################FDOSE NORMALISATION  ################################
 ##################################################################################
 
-number_4_dose = {'zero': '0',
- 'une ': '1 ',
- 'un ': '1 ',
- 'deux': '2',
- 'trois': '3',
- 'quatre': '4',
- 'cinq': '5',
- 'six': '6',
- 'sept': '7',
- 'huit': '8',
- 'neuf': '9',
- 'dix': '10',
- 'onze': '11',
- 'douze': '12',
- 'treize': '13',
- 'quatorze': '14',
- 'quinze': '15',
- 'seize': '16',
- 'dix-sept': '17',
- 'dix-huit': '18',
- 'dix-neuf': '19',
- #autre
-"fois ": "x ",
-"double":"2",
-"triple": "3"
-         }
-
-units = OrderedDict({
-    "mg/kg":["mg kg"],
-    "mg/ml":["mg ml"],
-    "µg/kg":["[µμ]g kg", "mcg kg"],
-    "µg/ml":["[µμ]g ml", "mcg ml"],
-
-    "unit/kg":["goutte par kg"],
-    "%":["%", "pourcent"],
-    "mg":["mg", "milligrammes?"],
-    "ml":["ml", "millilitres?"],
-    "unit":["cps?", "comprimes?", "cpr",
-          "ampoules?", "amp",
-            "doses?",
-          "injections?", 
-          "applications?", "patchs?", "tubes?",
-          "gell?ule?",
-          "bouffees?", 
-          "gouttes?", "gttes?", 
-          "flacon",
-          "sachet",
-          "ui", "unites?",
-          "pulverisations?", "inhalations?",
-          "perfusions?", "bolus",
-            "cycle", "cure"
-           ],
-    "g":["g", "grammes?"],
-    "µg":["[µμ]g", "microgrammes?", "mcg"],
-    "µmol":["[µμ]mol", "micromol", "mcmol"],
-    "kcal":["kcal", "kilocalories?"],
-    "cc":["cc", "centilitre"]
-        })
-
-unfolded_units = [t for sub in [[(k, u) for u in v] for k, v in units.items()] for t in sub]
+unfolded_units = [t for sub in [[(k, u) for u in v] for k, v in UNITS.items()] for t in sub]
 dose_regex = re.compile("(\d ?x ?)?(\d+[,\.]?\d{0,4})(x\d)?\s*("+ "|".join([t[1] for t in unfolded_units]) + ")?(x\d)?")
 
 
@@ -283,7 +115,7 @@ def unidecode_not(string, notunidecode="µμ"):
 
 def norm_unit(unit):
     if unit == "":
-        unit = "NA"
+        unit = None
     else:
         for key, pattern in unfolded_units:
             if re.match(pattern, unit):
@@ -305,7 +137,7 @@ def clean_dose(string):
     string = string.strip().lower()
     
     #replace number
-    for k, v in number_4_dose.items():
+    for k, v in NUMBER_4_DOSE.items():
         string = re.sub(k, v, string)
     #tokenization issue to fix using raw mention to be tokenization independent
     string = re.sub("(\d+) ?i?eme", '1', string) #tokenized 3eme
@@ -324,7 +156,5 @@ def clean_dose(string):
         value = norm_value(value, (modifier0, modifier1, modifier2))
         unit = norm_unit(unit)
         normed_dose.append("val:{}__{}".format(value, unit))    
-
-
 
     return "|".join(normed_dose)
