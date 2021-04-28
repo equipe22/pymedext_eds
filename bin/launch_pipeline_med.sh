726a9b61f701249f8caea489d0b9f3f31e934013
CalligerAlice <acalliger@bbs-edsgpu-p005.bbs.aphp.fr>
2021-04-28 12:55:27 +0200

    reorga code

--- bin/launch_pipeline_med.sh
+++ bin/launch_pipeline_med.sh
@@ -1,5 +1,6 @@
 ENV_PATH=/export/home/acalliger/pymedext-eds/med_env.tar.gz
 DATA_PATH=/export/home/acalliger/pymedext-eds/data.tar.gz
+MAIN_PATH=/export/home/acalliger/pymedext-eds/run_pipeline_med.py
 
 $SPARK_HOME/bin/spark-submit \
 --name pipeline_med \
@@ -12,6 +13,6 @@ $SPARK_HOME/bin/spark-submit \
 --conf spark.driver.memoryOverhead=5g \
 --conf "spark.executor.extraJavaOptions=-Dhdp.version=2.6.5.0-292" \
 --conf "spark.driver.extraJavaOptions=-Dhttp.proxyHost=proxym-inter.aphp.fr -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxym-inter.aphp.fr -Dhttps.proxyPort=8080" \
-run_ray.py --input-table "coronaomop_unstable.note" --output-table "note_nlp_medoc" --write-mode "full" --process "cuda" --limit 100 --num-replica 2 --num-gpus 1 --doc-batch-size 10 --sentence_batch_size 128
+$MAIN_PATH --input-table "coronaomop_unstable.note" --output-table "note_nlp_medoc" --write-mode "full" --process "cuda" --limit 100 --num-replica 2 --num-gpus 1 --doc-batch-size 10 --sentence_batch_size 128
 
 
---
+++ dag/med_pipeline_dev.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import timedelta
+from airflow.utils.dates import days_ago
+import os
+
+work_path = "/export/home/edsdev/app/bigdata/pymedext/"
+main_path = os.path.join(work_path, 'lib/run_ray.py')
+conf_path = os.path.join(work_path, "conf_dev.cf")
+run_path_match = os.path.join(work_path, 'bin/launch_pipeline_med.sh')
+env_path = os.path.join(work_path, 'envs/med_env.tar.gz')
+
+default_args = {
+    'owner': 'airflow',
+    'catchup': False,
+    'start_date': days_ago(2),
+    'retries': 0,
+}
+
+
+dag = DAG('Detection_Medicaments', description='detection medicaments',
+          catchup=False,
+          schedule_interval='@once',
+          default_args=default_args,
+          )
+
+with dag:
+    match_sivic = BashOperator(
+        task_id='detect_med',
+        bash_command="sh" + " "+ run_path_match + " " + main_path + " " + conf_path + " " + env_path,
+        dag=dag,
+    )
+
