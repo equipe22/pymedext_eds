19cd0b0751740f5cf5896d56a312aad578ee4c61
Alice Calliger <alice.calliger-ext@aphp.fr>
2021-04-29 13:18:26 +0000

    Update launch_pipeline_med.sh

--- bin/launch_pipeline_med.sh
+++ bin/launch_pipeline_med.sh
@@ -6,12 +6,12 @@ ENV_PATH=$3
 
 $SPARK_HOME/bin/spark-submit \
 --name pipeline_med \
---master local[2] \
---driver-memory=5g \
---executor-memory=5g \
+--master local[5] \
+--driver-memory=10g \
+--executor-memory=10g \
 --conf spark.sql.session.timeZone=Europe/Paris \
 --conf spark.ui.enabled=true \
---conf spark.driver.memoryOverhead=5g \
+--conf spark.driver.memoryOverhead=10g \
 --conf "spark.driver.extraJavaOptions=-Dhttp.proxyHost=proxym-inter.aphp.fr -Dhttp.proxyPort=8080 -Dhttps.proxyHost=proxym-inter.aphp.fr -Dhttps.proxyPort=8080" \
 --archives $ENV_PATH#med_env \
 --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./med_env/bin/python \
