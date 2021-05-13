fde68c43ddf095c8c5977e7bf775c4180274edd2
Alice Calliger <alice.calliger-ext@aphp.fr>
2021-05-13 17:41:57 +0000

    Update create_dataset.py

--- create_dataset/create_dataset.py
+++ create_dataset/create_dataset.py
@@ -7,7 +7,7 @@ spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 sql = spark.sql
 
 df_old_note = sql("select distinct note_id from coronaomop_unstable.note_nlp_medoc_v2")
-df_note = sql(f"select person_id, note_datetime, note_id, note_text, note_class_source_value from coronaomop_unstable.note")
+df_note = sql("select person_id, note_datetime, note_id, note_text, note_class_source_value from coronaomop_unstable.note")
 df_note = (
     df_note
     .dropna(subset="note_text")
