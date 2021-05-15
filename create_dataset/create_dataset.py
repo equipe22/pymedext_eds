f685316fb9e53140ffda3d129aae8221793a51a5
Alice Calliger <alice.calliger-ext@aphp.fr>
2021-05-15 16:48:40 +0000

    Update create_dataset.py

--- create_dataset/create_dataset.py
+++ create_dataset/create_dataset.py
@@ -13,7 +13,7 @@ df_note = (
     .dropna(subset="note_text")
     .join(df_old_note, on="note_id", how="left_anti")
     .select("person_id", "note_datetime", "note_id", "note_text", "note_class_source_value")
-    .limit(500000)
+    .limit(400000)
 )
     
 df_note.write.mode("overwrite").parquet('medicaments_tmp/subset_df.parquet')
