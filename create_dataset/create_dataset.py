ab886ff642f9cada8f8d23fd04ea54f0fb5c8214
Alice Calliger <alice.calliger-ext@aphp.fr>
2021-05-18 14:24:46 +0000

    Update create_dataset.py

--- create_dataset/create_dataset.py
+++ create_dataset/create_dataset.py
@@ -2,20 +2,6 @@ from pyspark.sql import functions as F
 from pyspark.sql import types as T
 from pyspark import SparkConf, SparkContext
 from pyspark.sql.session import SparkSession
-import sys
-from configparser import ConfigParser, ExtendedInterpolation
-from pathlib import Path
-
-if len(sys.argv) > 1:
-    conf_path = Path(sys.argv[1])
-else:
-    print('ERROR: configuration file path is not defined !')
-    sys.exit(1)
-
-# Load conf file
-config_object = ConfigParser(interpolation=ExtendedInterpolation())
-config_object.read(conf_path)
-limit = int(config_object["INPUT"]["limit"])
 
 spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 sql = spark.sql
@@ -27,7 +13,7 @@ df_note = (
     .dropna(subset="note_text")
     .join(df_old_note, on="note_id", how="left_anti")
     .select("person_id", "note_datetime", "note_id", "note_text", "note_class_source_value")
-    .limit(limit)
+    .limit(400000)
 )
     
 df_note.write.mode("overwrite").parquet('medicaments_tmp/subset_df.parquet')
